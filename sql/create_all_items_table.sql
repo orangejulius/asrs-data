3900dcd0989c5389dcba4c3f6fe8b9f9fef4e3a5
Julian Simioni <julian@simioni.org>
2015-11-08 14:03:12 -0500

    Add SQL files for Postgres schema

---
+++ sql/create_all_items_table.sql
@@ -0,0 +1,9 @@
+create table "all_items"
+(
+	item_id integer,
+	entity character(30),
+	enumerator integer,
+	attribute character(65),
+	value character(160),
+	display_value character(65)
+);
---
+++ sql/create_text_table.sql
@@ -0,0 +1,12 @@
+create table "text"
+(
+	item_id integer,
+	entity character(30),
+	enumerator integer,
+	attribute character(65),
+	text text,
+	item_year character(4),
+	item_month character(2),
+	item_date date,
+	text_key integer
+);
